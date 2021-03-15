d97884158591b186fdf20746a2c337a7bf90d32b
kinokoTakenoko35 <kinoko35@me.com>
2021-03-15 20:39:37 +0900

    responsive

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
 //burger
-document.getElementById("navBtn").onclick = function () {
-  document.querySelector("html").classList.toggle("open");
+// document.getElementById("navBtn").onclick = function () {
+//   document.querySelector("html").classList.toggle("open");
+// };
+
+// webpack.config.js
+var webpack = require("webpack");
+
+module.exports = {
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      IScroll: "iscroll",
+    }),
+  ],
 };
+
+// your-project.js
+var IScroll = require("iscroll");
+var $ = require("jquery");
+require("jquery-drawer");
+
+$(".drawer").drawer();
+
+// your-project.js
+IScroll = require("iscroll");
+var $ = (jQuery = require("jquery"));
+require("jquery-drawer");
+
+$(".drawer").drawer();
